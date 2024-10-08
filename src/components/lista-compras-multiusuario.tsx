a30f663f52b3c37bf0752493064aa3637d7a1e73
NotSleep <danielfranconot@gmail.com>
2024-10-08 17:18:50 -0300

    volviendo al inicio

--- src/components/lista-compras-multiusuario.tsx
+++ src/components/lista-compras-multiusuario.tsx
@@ -718,7 +718,7 @@ export default function ListaComprasOtgwMejorada() {
           </Dialog>
         </div>
         <div className="mt-8 text-center opacity-75">
-          <p className={darkMode ? 'text-[#D2B48C]' : 'text-[#8B4513]'}>Lista de Compras Para comprar cositas con el amor de mi vida Brena"</p>
+          <p className={darkMode ? 'text-[#D2B48C]' : 'text-[#8B4513]'}>Lista de Compras Para comprar cositas con el amor de mi vida Brena</p>
         </div>
       </div>
     </TooltipProvider>
